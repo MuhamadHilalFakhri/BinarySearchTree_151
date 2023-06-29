9e84b4ea8f9a1034cc02fa7ed73af1e1a73b9f88
hilal555 <muhamadhilal04@gmail.com>
2023-06-29 14:47:01 +0700

    If the value in the data field of the new node is greater than that of the parent

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -44,5 +44,11 @@ public:
 			ROOT = newNode; // Mark the new node as ROOT
 			return; //exit
 		}
+
+		if (element < parent->info)  //if the value in the data field of the new node is less than that of the paren
+		{
+			parent->leftchild = newNode; //Make the left child of the parent point to the new node
+		}
+		else if (element > parent->info) // If the value in the data field of the new node is greater than that of the parent
 	}
 };
